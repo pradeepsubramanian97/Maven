f716cf4d49f93f32292fa19f3711c277747e354f
pradeep_subramanian9 <pradeep.subramanian97@gmail.com>
2017-07-20 03:19:29 +0000

    Changed comment in print msg

--- src/main/java/com/mkyong/common/HelloDate.java
+++ src/main/java/com/mkyong/common/HelloDate.java
@@ -17,7 +17,7 @@ public class HelloDate {
      *  @return No value is returned
      */
     public static void main (String[] args) {
-	System.out.println ("Hello, it's: ");
+	System.out.println ("Hello, it's : ");
 	System.out.println(new Date());
     }
 }
